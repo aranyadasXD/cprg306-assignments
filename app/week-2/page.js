2c71ab286dce22c52b915f2c64e9d45efb9a5d69
Aranya Das <114148810+aranyadasXD@users.noreply.github.com>
2024-09-24 23:58:25 -0600

    Final (Maybe)

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,17 @@
-export default function Page() {
-    return (
-      <main>
-        <h1>Shopping List</h1>
-      </main>
-    );
-  }
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Aranya Das</h1>
+      <p>
+        <Link href="https://github.com/aranyadasXD/cprg306-assignments" passHref>
+        GitHub Repo
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,11 +4,10 @@ import Link from 'next/link';
 const StudentInfo = () => {
   return (
     <div>
-      <h1>Your Name</h1>
+      <h1>Aranya Das</h1>
       <p>
-        Check out my GitHub repository:{" "}
         <Link href="https://github.com/aranyadasXD/cprg306-assignments" passHref>
-          GitHub Repo
+        GitHub Repo
         </Link>
       </p>
     </div>

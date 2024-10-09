5bdd4b2f148956a01ee94c5bbad974260e0d5121
Aranya Das <114148810+aranyadasXD@users.noreply.github.com>
2024-10-08 22:53:48 -0600

    Almost done

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NewItem from './new-item';
+
+export default function Page() {
+  return (
+    <main className=" bg-neutral-900 min-h-screen">
+      <NewItem />
+    </main>
+  );
+}

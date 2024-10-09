0b52a73acf053392acfad9399f0f2614c9fdc659
Aranya Das <114148810+aranyadasXD@users.noreply.github.com>
2024-10-08 22:25:08 -0600

    start

--- app/page.js
+++ app/page.js
@@ -15,7 +15,11 @@ export default function HomePage() {
           Week 3 Assignment
         </Link>
       </p>
+      <p>
+        <Link href="/week-4" passHref>
+          Week 4 Assignment
+        </Link>
+      </p>
     </main>
   );
 }
-
